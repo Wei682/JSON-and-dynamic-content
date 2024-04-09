3525e79335dcef257d79856001687aaeca77f64e
Wei682 <xiawei29@hotmail.com>
2024-04-08 21:15:00 -0400

    Completed four restaurants

--- script.js
+++ script.js
@@ -8,20 +8,72 @@ let restaurantGridElement = document.getElementById('restaurantGrid');
 let restaurantDatabase = [
   {
     "Name" : "Daigo Sushi Roll Bar",
-    "Location" : "Downtown Brooklyn",
+    "Location" : "Brooklyn",
     "Address" : "Dekalb Market Hall, 445 Albee Square W, Brooklyn, NY 11201",
     "Category": "Japanese",
+    // Image is from https://daigobrooklyn.com/catering/
     "Website" : "https://daigobrooklyn.com/",
-    "Image": ""
+    "Image": "img/Daigo.jpg"
   },
   {
-    "Name" : "AAA",
+    "Name" : "DIG",
+    "Location" : "Brooklyn",
+    "Address" : "138 Willoughby St, Brooklyn, NY 11201",
+    "Category": "American",
+    "Website" : "https://www.diginn.com/city-point",
+    // Image is from https://www.downtownbrooklyn.com/listing/dig/
+    "Image": "img/DIG.jpeg"
+  },
+  {
+    "Name" : "Kong Sihk Tong",
+    "Location" : "Manhattan",
+    "Address" : "65 Bayard St, New York, NY 10013",
+    "Category": "Chinese",
+    "Website" : "https://kongsihktong.com/",
+    // Image is from https://kongsihktong.com/
+    "Image": "img/Kong Sihk Tong.png"
+  },
+  {
+    "Name" : "Han Dynasty",
+    "Location" : "Brooklyn",
+    "Address" : "Dekalb Market Hall, 1 Dekalb Ave, Brooklyn, NY 11201",
+    "Category": "Chinese",
+    "Website" : "https://www.handynasty.net/location/han-dynasty-brooklyn/",
+    // Image is from https://www.downtownbrooklyn.com/listing/han-dynasty/
+    "Image": "img/Han Dynasty.jpeg"
+  },
+  {
+    "Name" : "汉堡店",
     "Location" : "",
     "Address" : "",
     "Category": "",
     "Website" : "",
     "Image": ""
-  }
+  },
+  {
+    "Name" : "five senses",
+    "Location" : "",
+    "Address" : "",
+    "Category": "",
+    "Website" : "",
+    "Image": ""
+  },
+  {
+    "Name" : "意大利饭店",
+    "Location" : "",
+    "Address" : "",
+    "Category": "",
+    "Website" : "",
+    "Image": ""
+  },
+  {
+    "Name" : "越南餐厅",
+    "Location" : "",
+    "Address" : "",
+    "Category": "",
+    "Website" : "",
+    "Image": ""
+  },
 ];
 
 
@@ -54,6 +106,21 @@ function createElementProper(incJSON) {
   newContentAddress.classList.add('restaurantAddress');
   newContentAddress.innerText = "Address: " + incJSON['Address'];
   newContentElement.appendChild(newContentAddress);
+
+  let newContentWebsite = document.createElement("p");
+  let newContentLink =  document.createElement("a");
+  newContentLink.href = incJSON['Website'];
+  newContentLink.textContent = "Visit their website";
+  newContentWebsite.appendChild(newContentLink);
+  newContentElement.appendChild(newContentWebsite);
+
+  let newImageContainer = document.createElement("div");
+  newImageContainer.classList.add("restaurantImageContainer");
+  let newImage = document.createElement("IMG");
+  newImage.classList.add("restaurantImage");
+  newImage.src = incJSON['Image'];
+  newImageContainer.appendChild(newImage);
+  newContentElement.appendChild(newImageContainer);
 
   /// Add the item to the page
   restaurantGridElement.appendChild(newContentElement);
